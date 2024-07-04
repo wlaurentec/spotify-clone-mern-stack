2e6e1a73150c8f616076b21f74b84888b385b510
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-07-04 17:24:07 -0500

    display album and song item

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -7,6 +7,7 @@ const Display = () => {
     <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
       <Routes>
         <Route path="/" element={<DisplayHome />} />
+        <Route path="/home" element={<h1>hola</h1>} />
       </Routes>
     </div>
   )
