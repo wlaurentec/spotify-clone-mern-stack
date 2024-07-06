013d5991aaea0c2c23844db6f5d57786717f6dca
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-07-06 13:31:41 -0500

    add seek song

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,7 +4,7 @@ import { PlayerContext } from "../context/PlayerContext";
 
 const Player = () => {
 
-  const { seekBar, seekBg, playStatus, play, pause, track, time, previous, next  } = useContext(PlayerContext);
+  const { seekBar, seekBg, playStatus, play, pause, track, time, previous, next, seekSong  } = useContext(PlayerContext);
 
 
   return (
@@ -28,7 +28,7 @@ const Player = () => {
         </div>
         <div className="flex items-center gap-5">
           <p>{time.currentTime.minute}:{time.currentTime.second}</p>
-          <div ref={seekBg} className="w-[60vw] max-w-[300px] bg-gray-300 rounded-full cursor-pointer">
+          <div ref={seekBg} onClick={seekSong} className="w-[60vw] max-w-[300px] bg-gray-300 rounded-full cursor-pointer">
             <hr ref={seekBar} className="h-1 border-none w-0 bg-green-800 rounded-full"/>
           </div>
           <p>{time.totalTime.minute}:{time.totalTime.second}</p>
--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -53,6 +53,9 @@ const PlayerContextProvider = (props) => {
     }
   };
 
+  const seekSong = async (e) => {
+   audioRef.current.currentTime = (e.nativeEvent.offsetX / seekBg.current.offsetWidth) * audioRef.current.duration;
+  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -87,7 +90,8 @@ const PlayerContextProvider = (props) => {
     pause,
     playWithId,
     previous,
-    next
+    next,
+    seekSong
   };
 
   return (
