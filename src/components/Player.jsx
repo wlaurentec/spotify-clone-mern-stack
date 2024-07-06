a4891cf586e98ad8bf40803f9d5399c691b3c70f
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-07-06 11:41:06 -0500

    add play an pause

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets, songsData } from "../assets/assets";
+import { PlayerContext } from "../context/PlayerContext";
 
 const Player = () => {
+
+  const { seekBar, seekBg, playStatus, play, pause  } = useContext(PlayerContext);
+
+
   return (
     <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
       <div className="hidden lg:flex items-center gap-4 ">
@@ -15,14 +20,16 @@ const Player = () => {
         <div className="flex gap-4">
           <img className="w-4 cursor-pointer" src={assets.shuffle_icon} alt="" />
           <img className="w-4 cursor-pointer" src={assets.prev_icon} alt="" />
-          <img className="w-4 cursor-pointer" src={assets.play_icon} alt="" />
+          {
+            playStatus ? <img onClick={pause} className="w-4 cursor-pointer" src={assets.pause_icon} alt="" /> : <img onClick={play} className="w-4 cursor-pointer" src={assets.play_icon} alt="" />
+          }
           <img className="w-4 cursor-pointer" src={assets.next_icon} alt="" />
           <img className="w-4 cursor-pointer" src={assets.loop_icon} alt="" />
         </div>
         <div className="flex items-center gap-5">
           <p>1:06</p>
-          <div className="w-[60vw] max-w-[300px] bg-gray-300 rounded-full cursor-pointer">
-            <hr className="h-1 border-none w-0 bg-green-800 rounded-full"/>
+          <div ref={seekBg} className="w-[60vw] max-w-[300px] bg-gray-300 rounded-full cursor-pointer">
+            <hr ref={seekBar} className="h-1 border-none w-0 bg-green-800 rounded-full"/>
           </div>
           <p>3:20</p>
         </div>
---
+++ src/context/PlayerContext.jsx
@@ -0,0 +1,56 @@
+import { createContext, useRef, useState } from "react";
+import { songsData } from "../assets/assets";
+
+export const PlayerContext = createContext();
+
+const PlayerContextProvider = (props) => {
+  const audioRef = useRef();
+  const seekBg = useRef();
+  const seekBar = useRef();
+
+  const [track, setTrack] = useState(songsData[0]);
+  const [playStatus, setPlayStatus] = useState(false);
+  const [time, setTime] = useState({
+    currentTime: {
+      second: 0,
+      minute: 0,
+    },
+    totalTime: {
+      second: 0,
+      minute: 0,
+    },
+  });
+
+  const play = () => {
+    audioRef.current.play();
+    setPlayStatus(true);
+  };
+
+  const pause = () => {
+    audioRef.current.pause();
+    setPlayStatus(false);
+  };
+
+
+  const contexValue = {
+    audioRef,
+    seekBg,
+    seekBar,
+    track,
+    setTrack,
+    playStatus,
+    setPlayStatus,
+    time,
+    setTime,
+    play,
+    pause,
+  };
+
+  return (
+    <PlayerContext.Provider value={contexValue}>
+      {props.children}
+    </PlayerContext.Provider>
+  );
+};
+
+export default PlayerContextProvider;
